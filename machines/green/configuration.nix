b3636aadabb935565b4bf6afb21834ec7d32fa0a
RuboGubo <ruben.john.ward@gmail.com>
2025-04-03 17:59:54 +0100

    work

--- machines/green/configuration.nix
+++ machines/green/configuration.nix
@@ -1,7 +1,29 @@
 {
-  imports = [
+   imports = [
+     ./hardware-configuration.nix
+     # contains your disk format and partitioning configuration.
+     ../../modules/disko.nix
+     # this file is shared among all machines
+     ../../modules/shared.nix
+     # enables GNOME desktop (optional)
+     ../../modules/gnome.nix
+   ];
 
-  ];
+   # Put your username here for login
+   users.users.user.name = "rubogubo";
 
-  # New machine!
+   # Set this for clan commands that use ssh
+   # If you change the hostname, you need to update this line to root@<new-hostname>
+   # This only works however if you have avahi running on your admin machine else use IP
+   clan.core.networking.targetHost = "root@172.16.26.107";
+
+
+   # Replace this __CHANGE_ME__ with the result of the lsblk command from step 1. 
+   disko.devices.disk.main.device = "/dev/disk/by-id/wwn-0x500a0751e4fb0c48";
+
+   # IMPORTANT! Add your SSH key here
+   # e.g. > cat ~/.ssh/id_ed25519.pub
+   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdUiRDXTrOKj5wP9Urqlg9Ke3caKaC06lRYzVmF4bpA GitLab" ];
+
+   # ...
 }
