de8797fb5ddd4b5055954146306638a3e22bf63a
RuboGubo <ruboguboplays@gmail.com>
2025-08-21 01:54:23 +0100

    Refactor disk configuration for green machine
    Update disko.nix to define SSD and HDD devices with explicit partition
    layouts and mountpoints. Remove shared module import and old disk device
    setting from configuration.nix.

--- machines/green/configuration.nix
+++ machines/green/configuration.nix
@@ -1,9 +1,7 @@
 {
   imports = [
-    ../../modules/shared.nix
     ./disko.nix
   ];
-  disko.devices.disk.main.device = "/dev/nvme0n1";
   networking.useNetworkd = true; # Need this to fix a very random bug.
   
   clan.core.settings.state-version.enable = true;
