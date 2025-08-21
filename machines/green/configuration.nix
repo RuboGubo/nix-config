66480b933b2758cc0e5ee54a69d1cc22fb3b7648
RuboGubo <ruboguboplays@gmail.com>
2025-08-21 02:46:48 +0100

    Set GRUB device to /dev/sda using mkDefault

--- machines/green/configuration.nix
+++ machines/green/configuration.nix
@@ -1,8 +1,9 @@
-{
+{lib, ...}: {
   imports = [
     ./disko.nix
   ];
   networking.useNetworkd = true; # Need this to fix a very random bug.
+  boot.loader.grub.device = lib.mkDefault "/dev/sda";
   
   clan.core.settings.state-version.enable = true;
 }
