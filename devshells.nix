ae95e9e79908f3ea14213e3b4bd1796396b0be02
RuboGubo <ruben.john.ward@gmail.com>
2025-08-29 23:35:18 +0100

    Remove nil and nginx-language-server packages and update

--- devshells.nix
+++ devshells.nix
@@ -6,22 +6,20 @@ _: {
         default = pkgs.mkShellNoCC {
           packages = [
             inputs'.clan-core.packages.default
-            pkgs.nil
             pkgs.compose2nix
             pkgs.just
-            pkgs.nginx-language-server
             pkgs.systemctl-tui
           ];
         };
         debug = pkgs.mkShell {
-            packages = [
-                pkgs.python3
-                pkgs.helix
-            ];
-            shellHook = ''
-                    export GIT_ROOT="$(git rev-parse --show-toplevel)"
-                    export PATH=$PATH:~/Projects/clan-core/pkgs/clan-cli/bin
-            '';
+          packages = [
+            pkgs.python3
+            pkgs.helix
+          ];
+          shellHook = ''
+            export GIT_ROOT="$(git rev-parse --show-toplevel)"
+            export PATH=$PATH:~/Projects/clan-core/pkgs/clan-cli/bin
+          '';
         };
       };
     };
