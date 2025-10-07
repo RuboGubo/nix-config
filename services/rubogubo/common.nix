6883838e478174aaa905adf2c6e82ec244ac9d16
RuboGubo <ruben.john.ward@gmail.com>
2025-10-07 13:28:18 +0100

    add cachix to rubogubo

--- services/rubogubo/common.nix
+++ services/rubogubo/common.nix
@@ -21,6 +21,7 @@
 
   # Should be it's own service at some point
   nix.settings.substituters = [ "https://aseipp-nix-cache.global.ssl.fastly.net" ];
+  nix.settings.trusted-users = [ "rubogubo" ];
 
   programs.zsh.enable = true;
 
