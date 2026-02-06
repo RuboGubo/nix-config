b257487026de0166c281e3c84d4cfa2b28fcec35
Ruben Ward <ruben.john.ward@gmail.com>
2026-02-06 00:22:56 +0000

    get rid of spam

--- services/rubogubo/common.nix
+++ services/rubogubo/common.nix
@@ -102,6 +102,9 @@
         enable = true;
         nix-direnv.enable = true;
         enableZshIntegration = true;
+        config = {
+          hide_env_diff = true;
+        };
       };
     };
 }
