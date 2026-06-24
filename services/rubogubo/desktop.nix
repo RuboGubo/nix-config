53f911981db80ab531724c8424dd4e2a6a13a4fb
Ruben Ward <ruben.john.ward@gmail.com>
2026-06-24 18:55:47 +0100

    add nh

--- services/rubogubo/desktop.nix
+++ services/rubogubo/desktop.nix
@@ -59,6 +59,13 @@
       insomnia
     ];
 
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/home/rubogubo/Projects/clan-config"; # sets NH_OS_FLAKE variable for you
+    };
+
     programs.zed-editor = {
       package = pkgs.zed-editor-fhs;
       enable = true;
