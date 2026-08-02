dd7d4a67388e3300f59cf6d5fd07d704fe381602
Ruben Ward <ruben.john.ward@gmail.com>
2026-08-02 21:14:15 +0100

    disable borken driver

--- machines/green-laptop/configuration.nix
+++ machines/green-laptop/configuration.nix
@@ -15,7 +15,7 @@
   hardware.graphics.enable32Bit = true;
 
   # Use the Goodix driver module for Dell/Goodix hardware
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 
   swapDevices = [
     {
