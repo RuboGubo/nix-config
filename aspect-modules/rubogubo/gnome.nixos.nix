178aecad0afe81083ab55ee9372f7008f5b611aa
Ruben Ward <ruben.john.ward@gmail.com>
2026-08-04 14:06:57 +0100

    update default terminal

--- aspect-modules/rubogubo/gnome.nixos.nix
+++ aspect-modules/rubogubo/gnome.nixos.nix
@@ -12,7 +12,7 @@
       dconf.settings."org/gnome/shell".favorite-apps = lib.mkForce [
         "firefox.desktop"
         "app.zen_browser.zen.desktop"
-        "org.gnome.Console.desktop"
+        "com.raggesilver.BlackBox.desktop"
         "dev.zed.Zed.desktop"
         "org.gnome.Nautilus.desktop"
         "org.gnome.Calendar.desktop"
